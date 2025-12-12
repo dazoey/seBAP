636ae48b4251ff9ecf2737f9d9576bec15a964ce
dazoey <151373427+dazoey@users.noreply.github.com>
2025-12-12 19:26:11 +0700

    u

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 const ProfilePage = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <p className="mt-4">Jibran Analta Putra </p>
-      <p className="mt-4">21120123140170</p>
-      <p className="mt-4">Kelompok 29</p>
-      <p className="mt-4">shift 5</p>
+      <p className="mt-4">propil</p>
     </div>
   );
 };
